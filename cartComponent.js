a71fc4ff048a84268b15953c87bf91015adf6fc3
lotostoii <lotostoii@gmail.com>
2021-04-05 12:43:08 +1000

    some fixes

--- cartComponent.js
+++ cartComponent.js
@@ -1,10 +1,10 @@
 Vue.component('vertical-card', {
-    props: ['product'],
-    template:`
+  props: ['product'],
+  template: `
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-            <img :src="src" alt="image">
+            <img :src="product.src" alt="image">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
@@ -21,57 +21,45 @@ Vue.component('vertical-card', {
         </div>
     </div>
     `,
-    computed: {
-        src() {
-            return "https://picsum.photos/130/228?random=" + (Math.random()*1000);
-        },
-        total() {
-            return this.product.quantity*this.product.price;
-        }
-    }
-});
+  computed: {
+    total() {
+      return this.product.quantity * this.product.price
+    },
+  },
+})
 
 Vue.component('cart', {
-    template: `
+  template: `
         <div class="cart" style="gap: 20px;padding: 30px">
             <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search" @input="onSearch($event.target.value)">
-            <vertical-card v-for="product of products" :key="product.id_product" :product="product" @remove="remove(product)">
+            <vertical-card v-for="product of cartProducts" :key="product.id_product" :product="product" @remove="remove(product)">
             </vertical-card>
         </div>
     `,
-    data() {
-        return {
-            url: 'https://raw.githubusercontent.com/aminaev1311/online-store-api/master/responses/getBasket.json',
-            products: [],
-        }
+    
+  props: {
+    cartProducts: {
+      type: Array,
+      required: true,
     },
-    methods: {
-        getData(url) {
-            console.log("fetching data from" + this.url );
-            return fetch(url)
-            .then(response => {
-                console.log(response);
-                return response.json();
-            })
-            .then(data => {
-                this.products = data.contents;
-                console.log(this.products);
-            })
-            .catch(err => console.log(err));
-        },
-        remove(product) {
-            product.quantity--;
-            let index = this.products.findIndex( p=> p.id_product === product.id_product);
-            if (product.quantity===0) {
-                this.products.splice(index,1);
-            }
-        },
-        // //доделать метод добавления
-        // add(product) {
+  },
 
-        // }
+  methods: {
+    async addToCart(product) {
+      const isProd = this.cartProducts.find(({ id_product }) => +id_product === +product.id_product)
+      if (isProd) {
+        isProd.quantity++
+      } else {
+        this.cartProducts.push({ ...product, quantity: 1 })
+      }
     },
-    mounted() {
-        this.getData(this.url);
-    }
-});
+
+    remove(product) {
+      product.quantity--
+      let index = this.cartProducts.findIndex((p) => p.id_product === product.id_product)
+      if (product.quantity === 0) {
+        this.cartProducts.splice(index, 1)
+      }
+    },
+  },
+})
